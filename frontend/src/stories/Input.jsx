3596fedafc30a97493cda687e31d4cb59608d162
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-11-21 16:59:59 -0300

    storybook

--- frontend/src/stories/Input.jsx
+++ frontend/src/stories/Input.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export const Input = ({ primary, backgroundColor, size, label, ...props }) => {
     return (
-        <input type="text" placeholder="Enter your name" />
+        <input type="text" placeholder="Enter your name" {...props} />
     );
 };
   
