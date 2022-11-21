abdcb9fd064fdd90efe9cae6025b6739d16feaf7
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-11-21 17:33:59 -0300

    querys

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-
+import { useGetPokemonByNameQuery } from './services/pokemon';
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
   return (
     <div className="App">
       Hola Mundo
